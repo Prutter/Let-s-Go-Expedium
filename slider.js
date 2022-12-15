1ac569d2a29a7e3d55ad90d1cf6955dd44bae709
Kashish <kashishbansal044@gmail.com>
2022-12-15 22:42:56 +0530

    day-3 0546

---
+++ slider.js
@@ -0,0 +1,137 @@
+let objdata= [
+	{
+		i:1,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:1,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:1,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:1,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:0,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:1,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:0,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	},
+	{
+		i:1,
+		title : "Andaz Delhi",
+		location : "Delhi",
+		description : "Luxury hotel By Hyatt, near New-Delhi offering spa, bar, transport.",
+		price : 2250,
+		rating : 5.5,
+		offer : "15% offer deal",
+		photos :[
+			"https://a.travel-assets.com/media/meso_cm/PAPI/Images/hotels/14000000/13240000/13235700/13235605/5be22f44_b.jpg",
+		"https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/f22c2768.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c24fa57a.jpg?impolicy=resizecrop&rw=455&ra=fit","https://images.trvl-media.com/hotels/14000000/13240000/13235700/13235605/c31bea52.jpg?impolicy=resizecrop&rw=455&ra=fit"
+		]
+	}
+]
+
+let el = (tag)=>{
+	return document.createElement(tag);
+}
+
+let renderDom = (data)=>{
+    let am = document.querySelector(".container");
+    am.innerHTML = null;
+    data.forEach((ele)=>{
+        let div = el("div");
+        let div1 = el("div");
+        let image = el("img");
+        image.src= ele.photos[0];
+        let div2 = el("div");
+        let h2 = el("h2");
+        h2.innerHTML = ele.title
+        let h3 = el("h3")
+        h3.innerHTML = ele.location
+        let span = el("span");
+        span.innerHTML = ele.rating+ " &#9733;";
+        let button = el("button");
+        button.innerHTML = '&#8377; '+ele.price;
+        div1.append(image);
+        div2.append(h2,h3,span,button);
+        div.append(div1,div2);
+
+        am.append(div);
+    })
+}
+
+renderDom(objdata);
