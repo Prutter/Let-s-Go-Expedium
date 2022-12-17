e3bdc89a336ed1b5ef0861ebd2358b446668f265
Manju Sai <you@example.com>
2022-12-17 19:12:02 +0530

    navbar

--- ravi.js
+++ ravi.js
@@ -1,9 +1,7 @@
-
-    const data=async()=>{
-        let query=document.getElementById("search").value
-     let res= await fetch(`https://new-gpqr.onrender.com/airports?q=${query}`)
-     let result = await res.json()
-     console.log(result)
-    }
+const data=async()=>{
+    let query=document.getElementById("search").value
+ let res= await fetch(`https://new-gpqr.onrender.com/airports?q=${query}`)
+ let result = await res.json()
+ console.log(result)
+}
     
-   
